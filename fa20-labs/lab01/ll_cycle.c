9c7e130731e13401a40f0bf4fe891fc1a45c50ca
Sean W <wanquanlin@MacBook-Pro.local>
2025-06-05 02:16:51 +0800

    Finish lab01

--- fa20-labs/lab01/ll_cycle.c
+++ fa20-labs/lab01/ll_cycle.c
@@ -1,7 +1,16 @@
-#include <stddef.h>
+# include <stddef.h>
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* your code here */
+    node *tortoise = head;
+    node *hare = head;
+
+    while (hare != NULL && hare->next != NULL) {
+        hare = hare->next->next;
+        tortoise = tortoise->next;
+        if (tortoise == hare) {
+            return 1;
+        }
+    }
     return 0;
 }
